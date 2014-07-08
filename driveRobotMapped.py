0f26ed310f4656142e598e19b12caf67845f40c0
omegabyte <willcbaker@gmail.com>
2014-07-08 12:01:50 -0200

    I put everything in the right place

--- driveRobotMapped.py
+++ driveRobotMapped.py
@@ -13,102 +13,78 @@ from armcontrol import RobotArm
 # So we can catch Ctrl+C
 import signal
 import sys
-import RPi.GPIO as GPIO
 
-greenLightPin=23
-blueLightPin=24
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(greenLightPin,GPIO.OUT)
-GPIO.setup(blueLightPin,GPIO.OUT)
 
 # The deadzone within which we ignore inputs, approximately 1/3 of total possible input
 DEADZONE = 12000
 
 "Fetch the name of the motor mapped to the 'key' axis"
 def getMotor(key):
-	return {
-		'X1':'base',
-		'Y1':'shoulder',
-		'Y2':'elbow',
-		'RT':'grip',
-		'LT':'grip',
-		'LB':'wrist',
-		'RB':'wrist'
-	}.get(key,None)
-
-def signal_handler(signal, frame):
-	#print "Stopping RobotArm Controller"
+	return {ller"
 	sys.exit(0)
 
 # Capture Ctrl+C so we can shut down nicely
 signal.signal(signal.SIGINT, signal_handler)
 
-#print "Starting RobotArm Controller"
-#print "Press Ctrl+C at any time to quit"
+print "Starting RobotArm Controller"
+print "Press Ctrl+C at any time to quit"
 
+		'X1':'base',
+		'Y1':'shoulder',
+		'X2':'wristrotate',
+		'Y2':'elbow',
+		'RB':'grip', #RB is close
+		'LB':'grip', #LB is open
+		'RT':'wrist', #RT is up
+		'LT':'wrist' #LT is down
+	}.get(key,None)
+
+def signal_handler(signal, frame):
+	print "Stopping RobotArm Controller"
 # Create our RobotArm
 arm = RobotArm.RobotArm()
-#GPIO.output(greenLightPin,GPIO.HIGH)
 
 # Our main event loop
 for event in xbox_read.event_stream(deadzone=DEADZONE):
-	#print "Xbox event: %s" % (event)	
-
-	#Special case for LB and RB to control wrist
-	if(event.key=='LB'):
-		direction=1
-	elif(event.key=='RB'):
-		direction=2
-
-	# Special-case the B button, if it's been pressed, then toggle the light
-	elif(event.key=='B'):
-		if(event.value>0):
-			arm.toggleLight()
-		continue
+	#print "Xbox event: %s" % (event)
 	
-	# Special-case the A button, if it's being held, then we want the light
-	elif(event.key=='A'):
-		if(event.value>0):
+	#Special case directions for RB and LB, as we want the grip to move depending on which is used
+	if(event.key=='RB'):
+		if(event.value>0)
 			direction=1
 		else:
 			direction=0
-		arm.setLight(direction)
-		continue
+	elif(event.key=='LB'):
+		if (event.value>0)
+			direction=-1
+		else:
+			direction=0
 
-	# Special case directions for RT and LT, as we want the grip to move depending on which is used
-	elif(event.key=='RT'):
+	# Special case directions for RT and LT, as we want the wrist to move depending on which is used
+	if(event.key=='RT'):
 		direction=1
 	elif(event.key=='LT'):
 		direction=2
 	else:
 		# For all other cases, base direction on the value (negative = up, positive = down)
 		if(event.value<0):
-			direction = 2
+			direction = -1
 		elif(event.value>0):
 			direction = 1
 	
 	# If our axis has returned to zero, then stop moving
 	if(event.value==0):
 		direction = 0
-	#	j=1
-	#	k=0
 
 	# Fetch the motor to act on
 	motor = getMotor(event.key)
+
 	if(motor is not None):
 		try:
 			# Pass on the instruction to the RobotArm
 			arm.moveMotor(motor,direction)
-		#	if direction==0:
-		#		j=1
-		#		k=0
-		#	else:
-		#		j=0
-		#		k=1
 		except:
-			#print "Could not issue command to RobotArm"
+			print "Could not issue command to RobotArm"
 			raise
-	#GPIO.output(greenLightPin,j)
-	#GPIO.output(blueLightPin,k)
+	else:
+		print "I don't know how to move motor '%s'" % (motor) 
