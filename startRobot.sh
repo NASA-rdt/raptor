c9b76b0a1cd95a8962a1caacf0ceb6754f91f5c8
opiclops9 <pi@raspberrypi.(none)>
2014-10-01 20:17:36 +0000

    Added bash script to start python scripts

---
+++ startRobot.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+sudo python /home/pi/RobotArm/lightcontrol/controllerUSBStatus.py &
+sudo python /home/pi/RobotArm/SpaceNavControl.py &
+sudo python /home/pi/RobotArm/driveRobotMapped.py &
+
+exit 0
