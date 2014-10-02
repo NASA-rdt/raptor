404b1a35db44d523f3b769dee9541aef989705b7
opiclops9 <aaronbneely@gmail.com>
2014-10-02 22:37:50 +0000

    updated

--- startRobot.sh
+++ startRobot.sh
@@ -1,7 +1,5 @@
 ﻿#!/bin/bash
 
 sudo python /home/pi/RobotArm/SpaceNavControl.py &
-
-sleep 10
 sudo python /home/pi/RobotArm/lightcontrol/controllerUSBStatus.py &
 sudo python /home/pi/RobotArm/driveRobotMapped.py &
